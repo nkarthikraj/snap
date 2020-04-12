98f20c48d8025c34f55eb482e8d050b972248170
Karthik Raj Narayanaswamy <KarthikRajNarayanaswamy@Karthiks-iMac.broadband>
2020-04-12 12:43:10 +0100

    Adding snapshot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,16 +14,56 @@ def filter_instances(project):
         instances = ec2.instances.all()
 
     return instances
-
+################################
 @click.group()
+def cli():
+    '''Show all groups'''
+################################
+@cli.group('volumes')
+def volumes():
+    '''Commands for volumes'''
+################################
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+################################
+@cli.group('instances')
 def instances():
     """Commands for intances"""
+############################
+@snapshots.command('list')
+@click.option('--project', default=None,
+              help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    '''List ec2 snapshots'''
 
+    instances = filter_instances(project)
+
+    for i in instances:
+       for v in i.volumes.all():
+           for s in v.snapshots.all():
+              print(','.join((s.id,v.id, i.id, s.state, s.progress, s.start_time.strftime("%c"))))
+    return
+################################
+@volumes.command('list')
+@click.option('--project', default=None,
+              help="Only insatnces for project (tag Project:<name>)")
+def list_volumes(project):
+    '''List ec2 volumes'''
+
+    instances = filter_instances(project)
+
+    for i in instances:
+       for v in i.volumes.all():
+        print(','.join((v.id, i.id, v.state, v.encrypted and "Encrypted" or "Not Encrpted")))
+    return
+
+################################
 @instances.command('list')
 @click.option('--project', default=None,
                help="Only insatnces for project (tag Project:<name>)")
 def list_instances(project):
-    '''List eC2 instances'''
+    '''List ec2 instances'''
 
     instances = filter_instances(project)
 
@@ -37,7 +77,7 @@ def list_instances(project):
                         tags.get('Project','<no project>')
                         )))
     return
-
+################################
 @instances.command('stop')
 @click.option('--project', default=None,
                help="Only insatnces for project (tag Project:<name>)")
@@ -51,6 +91,7 @@ def stop_instances(project):
         i.stop()
 
     return
+################################
 @instances.command('start')
 @click.option('--project', default=None,
                help="Only insatnces for project (tag Project:<name>)")
@@ -64,6 +105,6 @@ def stop_instances(project):
         i.start()
 
     return
-
+################################
 if __name__ == "__main__":
-    instances()
+    cli()
